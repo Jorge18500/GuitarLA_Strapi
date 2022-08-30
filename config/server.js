9aa9bdf1b1339b2c857eec0981c56a3a4a065a0d
Jorge18500 <jorge15navarro18@gmail.com>
2022-08-30 17:34:07 -0500

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f5cb9d8884af81e246964cab7c1a709'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eb39aaf2-5fa8-4ecc-af4e-0a0c71d52cc4'
+};
